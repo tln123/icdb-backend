4e5bdf99173b08dd7750a0d73174923b25d64206
Tom Sisserman <tsisserm@akamai.com>
2021-09-08 22:02:07 +0300

    repos little changes for spring data

--- src/main/java/com/tomsisserman/icdb/repositories/CourseRepository.java
+++ src/main/java/com/tomsisserman/icdb/repositories/CourseRepository.java
@@ -1,7 +1,12 @@
 package com.tomsisserman.icdb.repositories;
 
 import com.tomsisserman.icdb.entities.Course;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface CourseRepository extends CrudRepository<Course, Integer> {
+public interface CourseRepository extends PagingAndSortingRepository<Course, Integer> {
+
+    Page<Course> findByCourseTopicCode(@Param("code") String code, Pageable pageable);
 }
--- src/main/java/com/tomsisserman/icdb/repositories/CourseTopicRepository.java
+++ src/main/java/com/tomsisserman/icdb/repositories/CourseTopicRepository.java
@@ -2,9 +2,11 @@ package com.tomsisserman.icdb.repositories;
 
 import com.tomsisserman.icdb.entities.CourseTopic;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "topics", path = "topics")
 public interface CourseTopicRepository extends CrudRepository<CourseTopic, String> {
 
     Optional<CourseTopic> findByName(String name);
